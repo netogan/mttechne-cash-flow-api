ca0764e996160deeb0196494fc0b87864dc24142
Geraldo Neto <netogan@gmail.com>
2024-08-09 12:00:53 -0300

    feature: add testes and readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# mttechne-cash-flow-api
+# Cashflow-Api
+
+The cash-flow-api module is responsible for managing cash flow transactions, persisting financial inflows and outflows.
+
+## Prerequisites
+- .Net 8
+
+## Instalation
+
+To install Winget (microsoft package manager) access the [link](https://learn.microsoft.com/pt-br/windows/package-manager/winget/#install-winget).
+
+
+Install .Net 8 SDK.
+```sh
+winget install --id=Microsoft.DotNet.SDK.8  -e
+```
+
+Build aplication in solution directory
+```sh
+dotnet build .\Cash.Flow.sln
+```
+
+Run aplication in solution directory in project directory
+```sh
+dotnet run --project .\src\Cash.Flow.Api\Cash.Flow.Api.csproj
+```
+
---
+++ tests/Cashflow.Api.Tests/Data/MigrationTests.cs
@@ -0,0 +1,46 @@
+﻿using Cash.Flow.Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cashflow.Api.Tests.Data
+{
+    public class MigrationTests : IDisposable
+    {
+        private CashFlowContext _context;
+
+
+        [Fact]
+        public async Task Should_ApplyWithSuccess()
+        {
+            string result;
+
+            var options = new DbContextOptionsBuilder<CashFlowContext>()
+                .UseSqlite("Data Source=:memory:")
+                .Options;
+
+            using (_context = new CashFlowContext(options))
+            {
+                DeleteDB();
+                _context.Database.OpenConnection();
+                _context.Database.Migrate();
+
+                using (var command = _context.Database.GetDbConnection().CreateCommand())
+                {
+                    command.CommandText = "SELECT 1 FROM sqlite_master WHERE type = 'table' AND tbl_name = 'Transactions'";
+                    result = command.ExecuteScalar().ToString();
+                }
+
+                _context.Database.CloseConnection();
+                DeleteDB();
+
+                Assert.True(result == "1");
+            }
+        }
+
+        private void DeleteDB() => _context.Database.EnsureDeleted();
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
